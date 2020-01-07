71ab05f9edb470ba001389dcde593299b707f4ed
Naresh Madiraju <naresh.johnny@gmail.com>
2020-01-07 06:03:35 -0500

    Added ec2_basic_sg resource

--- main.tf
+++ main.tf
@@ -12,3 +12,19 @@ terraform {
     region         = "ap-south-1"
   }
 }
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket         = "terraform-nxgcloud-infra-development"
+    key            = "global/infra/vpc.tfstate"
+    region         = "ap-south-1"
+  }
+}
+
+
+resource "aws_security_group" "ec2_basic_sg" {
+  name   = "ec2_basic_sg"
+  vpc_id = "${data.terraform_remote_state.vpc.outputs.vpc_id}"
+}
