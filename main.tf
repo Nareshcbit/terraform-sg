33fe1a2c13a4d15eecec0959f4f14924b9aaf778
Naresh Madiraju <naresh.johnny@gmail.com>
2020-01-06 21:36:44 -0500

    Initialized terrafomr

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws"{
+
+  region = var.region
+
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-nxgcloud-infra-development"
+    key            = "global/infra/sg.tfstate"
+    region         = "ap-south-1"
+  }
+}
