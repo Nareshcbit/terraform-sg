3098c2856f94d3d4b30f13ad0b738528eccd5359
Naresh Madiraju <naresh.johnny@gmail.com>
2020-01-08 18:06:21 -0500

    added security group rules

--- main.tf
+++ main.tf
@@ -23,8 +23,31 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
-
-resource "aws_security_group" "ec2_basic_sg" {
-  name   = "ec2_basic_sg"
+resource "aws_security_group" "bastion" {
+  name   = "Bastion"
   vpc_id = "${data.terraform_remote_state.vpc.outputs.vpc_id}"
+
+}
+
+resource "aws_security_group" "common" {
+  name   = "Common"
+  vpc_id = "${data.terraform_remote_state.vpc.outputs.vpc_id}"
+}
+
+resource "aws_security_group_rule" "allow_icmp_ingress" {
+  type              = "ingress"
+  from_port         = -1
+  to_port           = -1
+  protocol          = "icmp"
+  cidr_blocks       = var.desktop_cidr
+  security_group_id = "${aws_security_group.bastion.id}"
+}
+
+resource "aws_security_group_rule" "allow_icmp_egress" {
+  type              = "egress"
+  from_port         = -1
+  to_port           = -1
+  protocol          = "icmp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.bastion.id}"
 }
--- variables.tf
+++ variables.tf
@@ -1,1 +1,3 @@
 variable "region" { default = "ap-south-1"}
+
+variable "desktop_cidr" { default = ["0.0.0.0/0"] }
