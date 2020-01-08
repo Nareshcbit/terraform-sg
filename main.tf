5872de9f5f23bef8a69d7783d934153c7350b80d
Naresh Madiraju <naresh.johnny@gmail.com>
2020-01-08 18:21:35 -0500

    Bastion Security Group complete

--- main.tf
+++ main.tf
@@ -43,6 +43,15 @@ resource "aws_security_group_rule" "allow_icmp_ingress" {
   security_group_id = "${aws_security_group.bastion.id}"
 }
 
+resource "aws_security_group_rule" "allow_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = var.desktop_cidr
+  security_group_id = "${aws_security_group.bastion.id}"
+}
+
 resource "aws_security_group_rule" "allow_icmp_egress" {
   type              = "egress"
   from_port         = -1
@@ -50,4 +59,14 @@ resource "aws_security_group_rule" "allow_icmp_egress" {
   protocol          = "icmp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.bastion.id}"
+}
+
+resource "aws_security_group_rule" "bastion_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.bastion.id}"
+
 }
